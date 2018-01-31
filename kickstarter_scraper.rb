713b24f14e1c31661ae33dcb4a1323e0816423e3
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-31 16:01:56 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -27,6 +27,7 @@ def create_project_hash
       :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
       :description => project.css("p.bbcard_blurb").text,
       :location => project.css("span.location-name").text,
+      #we convert the percent funded to an integer for manipulation later on
       :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
     }
   end
